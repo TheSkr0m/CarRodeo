cad3be8e71b23ca1bb471c95ea918e767c50fc3a
Stasyuk Denis <stasyukda@gmail.com>
2019-01-29 21:03:49 +0200

    ui add

---
+++ Assets/Script/SaveDate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveDate 
+{
+    public GameManager gameManager;
+    public GamePanel gamePanel;
+
+    public SaveDate(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
+
+    public void Save()
+    {
+
+        PlayerPrefs.SetInt("Score", gameManager.player.score);
+        PlayerPrefs.SetInt("Money", gameManager.player.money);
+        PlayerPrefs.SetInt ("Time", gameManager.player.time);
+
+
+    }
+
+    
+
+
+}
